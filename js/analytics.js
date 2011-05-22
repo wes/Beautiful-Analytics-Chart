02c097324327766e2ec2cb17089b11b739324384
Sagie Maoz <sagie@maoz.info>
2011-05-22 12:03:10 +0300

    formatting code

--- js/analytics.js
+++ js/analytics.js
@@ -17,29 +17,6 @@ Raphael.fn.drawGrid = function(x, y, w, h, wv, hv, color) {
 
 function drawLine(conf) {
 
-	var getAnchors = function(p1x, p1y, p2x, p2y, p3x, p3y) {
-		var l1 = (p2x - p1x) / 2,
-			l2 = (p3x - p2x) / 2,
-			a = Math.atan((p2x - p1x) / Math.abs(p2y - p1y)),
-			b = Math.atan((p3x - p2x) / Math.abs(p2y - p3y)),
-			alpha, dx1, dy1, dx2, dy2;
-		
-		a = p1y < p2y ? Math.PI - a: a;
-		b = p3y < p2y ? Math.PI - b: b;
-		alpha = Math.PI / 2 - ((a + b) % (Math.PI * 2)) / 2;
-		dx1 = l1 * Math.sin(alpha + a);
-		dy1 = l1 * Math.cos(alpha + a);
-		dx2 = l2 * Math.sin(alpha + b);
-		dy2 = l2 * Math.cos(alpha + b);
-		
-		return {
-			x1: p2x - dx1,
-			y1: p2y + dy1,
-			x2: p2x + dx2,
-			y2: p2y + dy2
-		};
-	};
-
 	var holder = !conf.holder ? '': conf.holder,
 		data_holder = !conf.data_holder ? '': conf.data_holder,
 		mastercolor = !conf.mastercolor ? '#01A8F0': conf.mastercolor,
@@ -53,7 +30,29 @@ function drawLine(conf) {
 		data = [],
 		datatotal = [],
 		lines1 = [],
-		lines2 = [];
+		lines2 = [],
+		getAnchors = function(p1x, p1y, p2x, p2y, p3x, p3y) {
+			var l1 = (p2x - p1x) / 2,
+				l2 = (p3x - p2x) / 2,
+				a = Math.atan((p2x - p1x) / Math.abs(p2y - p1y)),
+				b = Math.atan((p3x - p2x) / Math.abs(p2y - p3y)),
+				alpha, dx1, dy1, dx2, dy2;
+
+			a = p1y < p2y ? Math.PI - a: a;
+			b = p3y < p2y ? Math.PI - b: b;
+			alpha = Math.PI / 2 - ((a + b) % (Math.PI * 2)) / 2;
+			dx1 = l1 * Math.sin(alpha + a);
+			dy1 = l1 * Math.cos(alpha + a);
+			dx2 = l2 * Math.sin(alpha + b);
+			dy2 = l2 * Math.cos(alpha + b);
+
+			return {
+				x1: p2x - dx1,
+				y1: p2y + dy1,
+				x2: p2x + dx2,
+				y2: p2y + dy2
+			};
+		};
 		
 	if (!$(data_holder)) {
 		return false;
